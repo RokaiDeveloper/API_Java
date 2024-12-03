dc28608e4a7fd7d697c26f7cc85b76118934b404
RokaiDeveloper <roger.edab@gmail.com>
2024-12-03 20:29:50 -0300

    Finalizando produto
    Criado o constructor vazio e o constructor com todos os campos e criados getters e setters

--- src/main/java/com/rogerbarreto/comanda/models/Produto.java
+++ src/main/java/com/rogerbarreto/comanda/models/Produto.java
@@ -1,14 +1,13 @@
 package com.rogerbarreto.comanda.models;
 
-import org.antlr.v4.runtime.misc.NotNull;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+
 
 @Entity
 @Table(name = "TABLE_NAME")
@@ -29,5 +28,52 @@ public class Produto {
     @Column(name = "pro_valor", nullable = false)
     private double pro_valor;
 
+ 
+    public Produto() {
+    }
+
+
+    public Produto(Long pro_id, String pro_descricao, String pro_und, double pro_valor) {
+        this.pro_id = pro_id;
+        this.pro_descricao = pro_descricao;
+        this.pro_und = pro_und;
+        this.pro_valor = pro_valor;
+    }
+
+
+    public Long getPro_id() {
+        return this.pro_id;
+    }
+
+    public void setPro_id(Long pro_id) {
+        this.pro_id = pro_id;
+    }
+
+    public String getPro_descricao() {
+        return this.pro_descricao;
+    }
+
+    public void setPro_descricao(String pro_descricao) {
+        this.pro_descricao = pro_descricao;
+    }
+
+    public String getPro_und() {
+        return this.pro_und;
+    }
+
+    public void setPro_und(String pro_und) {
+        this.pro_und = pro_und;
+    }
+
+    public double getPro_valor() {
+        return this.pro_valor;
+    }
+
+    public void setPro_valor(double pro_valor) {
+        this.pro_valor = pro_valor;
+    }
+
     
 }
+
+    
