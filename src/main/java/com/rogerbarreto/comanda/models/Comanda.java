1e5acf4765d1271d760d2cd0e9455a39847ed8c5
RokaiDeveloper <roger.edab@gmail.com>
2024-12-03 20:53:08 -0300

    Join
    Adicionado validação de join para comanda > mesa

--- src/main/java/com/rogerbarreto/comanda/models/Comanda.java
+++ src/main/java/com/rogerbarreto/comanda/models/Comanda.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
@@ -21,6 +22,7 @@ public class Comanda {
     private long com_id;
 
     @ManyToOne
+    @JoinColumn(name = "com_mes_id")
     @Column(name = "com_mes_id", nullable = false)
     private Mesa com_mes_id;
 
