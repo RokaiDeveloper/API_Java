164dd21ce013f4b2c823e29ece59259685c28a76
RokaiDeveloper <roger.edab@gmail.com>
2024-12-03 20:36:03 -0300

    criado tabela mesa
    Criado a tabela mesa, definido suas colunas, criado construtor vazio e construtor com todos os campos e também getters e setters

---
+++ src/main/java/com/rogerbarreto/comanda/models/Mesa.java
@@ -0,0 +1,66 @@
+package com.rogerbarreto.comanda.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TABLE_NAME")
+public class Mesa {
+    public static final String TABLE_NAME = "mesa";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "mes_id", unique = true)
+    private long mes_id;
+
+    @Column(name = "mes_numero", nullable = false)
+    private int mes_numero;
+
+    @Column(name = "mes_status", nullable = false)
+    private boolean mes_status;
+
+
+    public Mesa() {
+    }
+
+
+    public Mesa(long mes_id, int mes_numero, boolean mes_status) {
+        this.mes_id = mes_id;
+        this.mes_numero = mes_numero;
+        this.mes_status = mes_status;
+    }
+
+
+    public long getMes_id() {
+        return this.mes_id;
+    }
+
+    public void setMes_id(long mes_id) {
+        this.mes_id = mes_id;
+    }
+
+    public int getMes_numero() {
+        return this.mes_numero;
+    }
+
+    public void setMes_numero(int mes_numero) {
+        this.mes_numero = mes_numero;
+    }
+
+    public boolean isMes_status() {
+        return this.mes_status;
+    }
+
+    public boolean getMes_status() {
+        return this.mes_status;
+    }
+
+    public void setMes_status(boolean mes_status) {
+        this.mes_status = mes_status;
+    }
+
+}
