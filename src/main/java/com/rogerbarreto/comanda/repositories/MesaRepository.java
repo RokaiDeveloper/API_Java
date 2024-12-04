8c01fd7ba06377f4158558ed4303242dc0c365b3
RokaiDeveloper <roger.edab@gmail.com>
2024-12-04 20:47:17 -0300

    Metodo por numero
    Encontrar a mesa por numero criado no repositorio para chamar no service

--- src/main/java/com/rogerbarreto/comanda/repositories/MesaRepository.java
+++ src/main/java/com/rogerbarreto/comanda/repositories/MesaRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.rogerbarreto.comanda.models.Mesa;
+import java.util.List;
+
 
 @Repository
 public interface MesaRepository extends JpaRepository<Mesa,Long>{
     
+    List<Mesa> findByMes_numero(int mes_numero);
 }
