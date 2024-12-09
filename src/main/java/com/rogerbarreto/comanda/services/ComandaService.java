d494cc93075338ecea84def2515e8f829ff5e044
RokaiDeveloper <roger.edab@gmail.com>
2024-12-09 20:09:38 -0300

    Criado o service da comanda
    Criado service da comanda, com o find, create, update e delete, devo denotar aqui em nota para mim mesmo que o planejamento do projeto foi porco, o com_status teria que ser um enum representando o status e não um verdadeiro e falso, e posterior também planejar uma validação para achar a comanda por mesa (QUE SERIA O BÁSICO) validando as duas seguintes condições:
    - A comanda está com status aberto
    - Procura pelo NUMERO DA MESA
    
    obs adicionar o numero da mesa a estrutura de banco de dados e models  da comanda.

---
+++ src/main/java/com/rogerbarreto/comanda/services/ComandaService.java
@@ -0,0 +1,47 @@
+package com.rogerbarreto.comanda.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.rogerbarreto.comanda.models.Comanda;
+import com.rogerbarreto.comanda.repositories.ComandaRepository;
+
+public class ComandaService {
+    
+    @Autowired
+    private ComandaRepository comandaRepository;
+
+    public Comanda findComandaById(Long id)
+    {
+        Optional<Comanda> comanda = this.comandaRepository.findById(id);
+        return comanda.orElseThrow(()-> new RuntimeException("Comanda não foi encontrada pelo ID" + id ));
+    }
+
+    @Transactional
+    public Comanda createComanda(Comanda obj)
+    {
+        obj = this.comandaRepository.save(obj);
+        return obj;
+    }
+
+    @Transactional
+    public Comanda updateComanda(Comanda obj)
+    {
+        Comanda newObj = findComandaById(obj.getCom_id());
+        newObj.setCom_status(obj.getCom_status());
+        return this.comandaRepository.save(newObj);
+    }
+
+    public void deleteComanda(Long id)
+    {
+        findComandaById(id);
+        try
+        {
+            this.comandaRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException( "Não foi possivel realizar o delete");
+        }
+    }
+}
