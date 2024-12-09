82312ffe49dd7dc713373699c27d5a7fcec3f3c1
RokaiDeveloper <roger.edab@gmail.com>
2024-12-09 20:35:34 -0300

    criado serviço itemcomanda
    Criado parcial, metodo find por 2 campos e o create, falta o resto

---
+++ src/main/java/com/rogerbarreto/comanda/services/ItemComandaService.java
@@ -0,0 +1,33 @@
+package com.rogerbarreto.comanda.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.rogerbarreto.comanda.models.ItemComanda;
+import com.rogerbarreto.comanda.repositories.ItemComandaRepository;
+
+public class ItemComandaService {
+    
+    @Autowired
+    private ItemComandaRepository itemComandaRepository;
+
+    public ItemComanda findItemComandaByProCodigo(Long pro_codigo)
+    {
+        Optional<ItemComanda> comanda = this.itemComandaRepository.findByIco_pro_codigo(pro_codigo);
+        return comanda.orElseThrow(() -> new RuntimeException("Não foi possivel encontrar o item da comanda pelo código de produto : " + pro_codigo));
+    }
+
+    public ItemComanda findItemComandaById(Long id)
+    {
+        Optional<ItemComanda> itemComanda = this.itemComandaRepository.findById(id);
+        return itemComanda.orElseThrow(()-> new RuntimeException("Não foi possivel encontrar o item pelo id do item " + id));
+    }
+    @Transactional
+    public ItemComanda createItemComanda(ItemComanda obj)
+    {
+        obj = this.itemComandaRepository.save(obj);
+        return obj;
+    }
+}
