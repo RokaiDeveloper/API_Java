9a036fc8e43c24045df485ad4000a7f28627bb47
RokaiDeveloper <roger.edab@gmail.com>
2024-12-17 09:57:16 -0300

    ERREI
    Não era erro, era bola, docker do banco desligado

--- src/main/java/com/rogerbarreto/comanda/models/ItemComanda.java
+++ src/main/java/com/rogerbarreto/comanda/models/ItemComanda.java
@@ -18,27 +18,27 @@ public class ItemComanda {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ico_id", unique = true)
-    private long icoId;
+    private long ico_id;
 
     @ManyToOne
 
     @JoinColumn(name = "ico_com_id")
-    private Comanda icoComId;
+    private Comanda ico_com_id;
 
     @Column(name = "ico_pro_codigo")
-    private long icoProCodigo;
+    private long ico_pro_codigo;
 
     @Column(name = "ico_quantidade")
-    private double icoQuantidade;
+    private double ico_quantidade;
 
     @Column(name = "ico_valor")
-    private double icoValor;
+    private double ico_valor;
 
     @Column(name = "ico_und")
-    private String IcoUnd;
+    private String ico_und;
 
     @Column(name = "ico_datahora")
-    private LocalDateTime icoDataHora;
+    private LocalDateTime ico_datahora;
 
 
     public ItemComanda() {
@@ -46,70 +46,70 @@ public class ItemComanda {
 
 
     public ItemComanda(long ico_id, Comanda ico_com_id, long ico_pro_codigo, double ico_quantidade, double ico_valor, String ico_und, LocalDateTime ico_datahora) {
-        this.icoId = ico_id;
-        this.icoComId = ico_com_id;
-        this.icoProCodigo = ico_pro_codigo;
-        this.icoQuantidade = ico_quantidade;
-        this.icoValor = ico_valor;
-        this.IcoUnd = ico_und;
-        this.icoDataHora = ico_datahora;
+        this.ico_id = ico_id;
+        this.ico_com_id = ico_com_id;
+        this.ico_pro_codigo = ico_pro_codigo;
+        this.ico_quantidade = ico_quantidade;
+        this.ico_valor = ico_valor;
+        this.ico_und = ico_und;
+        this.ico_datahora = ico_datahora;
     }
 
 
     public long getIco_id() {
-        return this.icoId;
+        return this.ico_id;
     }
 
     public void setIco_id(long ico_id) {
-        this.icoId = ico_id;
+        this.ico_id = ico_id;
     }
 
     public Comanda getIco_com_id() {
-        return this.icoComId;
+        return this.ico_com_id;
     }
 
     public void setIco_com_id(Comanda ico_com_id) {
-        this.icoComId = ico_com_id;
+        this.ico_com_id = ico_com_id;
     }
 
     public long getIco_pro_codigo() {
-        return this.icoProCodigo;
+        return this.ico_pro_codigo;
     }
 
     public void setIco_pro_codigo(long ico_pro_codigo) {
-        this.icoProCodigo = ico_pro_codigo;
+        this.ico_pro_codigo = ico_pro_codigo;
     }
 
     public double getIco_quantidade() {
-        return this.icoQuantidade;
+        return this.ico_quantidade;
     }
 
     public void setIco_quantidade(double ico_quantidade) {
-        this.icoQuantidade = ico_quantidade;
+        this.ico_quantidade = ico_quantidade;
     }
 
     public double getIco_valor() {
-        return this.icoValor;
+        return this.ico_valor;
     }
 
     public void setIco_valor(double ico_valor) {
-        this.icoValor = ico_valor;
+        this.ico_valor = ico_valor;
     }
 
     public String getIco_und() {
-        return this.IcoUnd;
+        return this.ico_und;
     }
 
     public void setIco_und(String ico_und) {
-        this.IcoUnd = ico_und;
+        this.ico_und = ico_und;
     }
 
     public LocalDateTime getIco_datahora() {
-        return this.icoDataHora;
+        return this.ico_datahora;
     }
 
     public void setIco_datahora(LocalDateTime ico_datahora) {
-        this.icoDataHora = ico_datahora;
+        this.ico_datahora = ico_datahora;
     }
 
 }
